f26fd69cc4d5dc9750952fbafa526682d91d1a2b
Stenio Ferreira <stenio123@gmail.com>
2019-12-12 11:38:43 -0500

    Cambio squad 1

--- workflow/squad1/outputs.tf
+++ workflow/squad1/outputs.tf
@@ -4,6 +4,10 @@ output "output_from_random" {
   value = random_id.random.hex
 }
 
+output "output_from_random" {
+  value = "test"
+}
+
 # output "keepers" {
 #  value = random_id.random.keepers
 # }
