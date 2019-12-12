fe357c35de008c60d79d88884b1bd3dd48e08c23
Stenio Ferreira <stenio123@gmail.com>
2019-12-12 15:12:54 -0500

    Changes to code

--- workflow/squad1/main.tf
+++ workflow/squad1/main.tf
@@ -6,11 +6,11 @@ data "terraform_remote_state" "ip" {
 }
 module "module2" {
   source  = "app.terraform.io/TFE_PoV/module2/demo"
-  version = "1.0.1"
+  version = "1.0.2"
 
-  test_var = "test"
+  test_var = "teste"
 }
 
 resource "random_id" "random" {
-  byte_length = 12
+  byte_length = 8
 }
