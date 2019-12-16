34fb0ec36b85572180a22f3ce24aa4e0279453d5
Stenio Ferreira <stenio123@gmail.com>
2019-12-16 13:42:26 -0500

    Added logging and state

--- logs/audit.sh
+++ logs/audit.sh
@@ -1,7 +1,14 @@
-sudo docker ps
 sudo docker logs ptfe_atlas
 
-# Two options to consume logs:
-# sudo docker logs ptfe_atlas > tfe_logs.json
-# OR send to syslog using external tool like logspout
-# docker run --name="logspout"  --volume=/var/run/docker.sock:/var/run/docker.sock gliderlabs/logspout syslog+tls://YOUR_LOG_SOLUTION_URL:55555
+# Reference: https://www.terraform.io/docs/enterprise/admin/logging.html
+
+# Demo:
+# In a real world scenario, you would pipe the Docker logs to a Splunk, Datadog, Logstash, etc to parse the logs
+# For a simple demo we can look into the log files using jq and search vi:
+
+# Gets container id, so we can check the log file
+TFE_LOG_ID=$(sudo docker ps --no-trunc -aqf "name=ptfe_atlas")
+# Gets last 100 lines and exports to a file
+sudo tail -n 100  /var/lib/docker/containers/$TFE_LOG_ID/$TFE_LOG_ID-json.log > tst.log
+# Open the file, and use the command "/action" to validate a certain action made, like an apply, is logged
+vi tst.log
