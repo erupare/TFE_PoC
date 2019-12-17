65ad4f30b31802348072c6796448da6c0ab10e4f
Stenio Ferreira <stenio123@gmail.com>
2019-12-17 08:57:18 -0500

    Added output

--- vaultintegration/outputs.tf
+++ vaultintegration/outputs.tf
@@ -5,3 +5,7 @@ output "static_secret" {
 output "dynamic_secret" {
   value = data.vault_generic_secret.my_dynamic_secret
 }
+
+output "aws_external_ip" {
+  value = "${aws_instance.ubuntu.public_ip}"
+}
